c8202855568528a87edebf8f6e6c35ce9d7648c9
Gerd Stolpmann <gerd@figlyinc.com>
2023-02-28 17:02:10 +0100

    switching from wasmer to wasi-js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 'use strict';
 
-const { WASI } = require("@wasmer/wasi");
-const wasiBindings = require("@wasmer/wasi/lib/bindings/node");
+//import { WASI } from "wasi-js";
+//import wasiBindings from "wasi-js/dist/bindings/node";
+//import fs from 'fs';
+//import util from "util";
+//import process from "process";
+//import { execSync } from "child_process";
+
+const wasi_js = require("wasi-js");
+const WASI = wasi_js.default
+const bindings = require("wasi-js/dist/bindings/node");
+const wasiBindings = bindings.default
 const fs = require('fs');
 const util = require("util");
 const process = require("process")
@@ -103,92 +112,18 @@ function wasicaml_rename(session) {
 }
 
 
-let syscalls =
-    [ "args_get",
-      "args_sizes_get",
-      "environ_get",
-      "environ_sizes_get",
-      "clock_res_get",
-      "clock_time_get",
-      "fd_advise",
-      "fd_allocate",
-      "fd_close",
-      "fd_datasync",
-      "fd_fdstat_get",
-      "fd_fdstat_set_flags",
-      "fd_fdstat_set_rights",
-      "fd_filestat_get",
-      "fd_filestat_set_size",
-      "fd_filestat_set_times",
-      "fd_pread",
-      "fd_prestat_get",
-      "fd_prestat_dir_name",
-      "fd_pwrite",
-      "fd_read",
-      "fd_readdir",
-      "fd_renumber",
-      "fd_seek",
-      "fd_sync",
-      "fd_tell",
-      "fd_write",
-      "path_create_directory",
-      "path_filestat_get",
-      "path_filestat_set_times",
-      "path_link",
-      "path_open",
-      "path_readlink",
-      "path_remove_directory",
-      "path_rename",
-      "path_symlink",
-      "path_unlink_file",
-      "poll_oneoff",
-      "proc_exit",
-      "proc_raise",
-      "sched_yield",
-      "random_get",
-      "sock_recv",
-      "sock_send",
-      "sock_shutdown"
-    ]
-
 async function instantiate(wasm_mod, args) {
     const wasi = new WASI({
-        ...wasiBindings,
+        bindings: {...wasiBindings, fs},
         args: args,
         env: process.env,
-        preopenDirectories: { "/": "/" },
+        preopens: { "/": "/" },
     });
     let wasi_imports = wasi.getImports(wasm_mod).wasi_snapshot_preview1;
     let session =
         { instance: null };
-    let realimport = {};
-    for (let n of syscalls) {
-        let f = wasi_imports[n];
-        realimport[n] = ((...args) => {
-            //console.error("WASI syscall: " + n);
-            switch (n) {
-            case "path_open":
-                let open_path = get_string(session, args[2]);
-                // console.error("  ", open_path);
-                break;
-            case "path_rename":
-                let old_path = get_string(session, args[1]);
-                let new_path = get_string(session, args[4]);
-                // console.error("  ", old_path, "->", new_path);
-                break;
-            case "path_filestat_get":
-                let stat_path = get_string(session, args[2]);
-                // console.error("  ", stat_path);
-                break;
-            };
-            let r = f.apply(undefined, args);
-            //if (r != 0)
-            //    console.error("  code: ", r);
-            return r;
-        });
-    };
     const importObject =
-          { wasi_snapshot_preview1: realimport,
+          { wasi_snapshot_preview1: wasi_imports,
             wasicaml: { "wasicaml_try": wasicaml_try(session),
                         "wasicaml_try4": wasicaml_try4(session),
                         "wasicaml_throw": wasicaml_throw(session),
